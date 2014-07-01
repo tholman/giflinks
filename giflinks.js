288f6d87609fef5ae7960d95cf855aaeb1cd2a7d
Tim <timothy.w.holman@gmail.com>
2014-07-01 09:22:45 -0400

    basic link tracking

--- giflinks.js
+++ giflinks.js
@@ -9,13 +9,13 @@ var GifLinks = (function() {
   // Soft object augmentation
   function extend( target, source ) {
 
-      for ( var key in source )
-
-          if ( !( key in target ) )
-
-              target[ key ] = source[ key ];
-
-      return target;
+    for ( var key in source ) {
+      if ( !( key in target ) ) {
+        target[ key ] = source[ key ];
+      }
+    }
+    
+    return target;
   }
 
   // Applys a dict of css properties to an element
@@ -32,6 +32,40 @@ var GifLinks = (function() {
 
   // Initialize
   function init( elements ) {
+
+    if ( elements.length ) {
+
+      // Loop and assign
+      for( var i = 0; i < elements.length; i++ ) {
+        track( elements[ i ] );
+      }
+
+    } else {
+        track( elements );
+    }
+  }
+
+  function track( element ) {
+
+    // Only track if the element has a gif source!
+    if( element.getAttribute( 'data-src' ) ) {
+
+      element.addEventListener( 'mouseover', function() {
+        activate( this ); // Start the party!
+      },  false ); 
+
+      element.addEventListener( 'mouseout',  function() {
+        deactivate(); // End the party, aww :(
+      }, false); 
+    }
+  }
+
+  function activate( element ) {
+    console.log( "activate!", element )
+  }
+
+  function deactivate() {
+    console.log( "aww" )
   }
 
 
